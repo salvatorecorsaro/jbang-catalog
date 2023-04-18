c2d918c21bc631978dc1768a5b006a661f82e478
Salvatore Corsaro <55556636+salvatorecorsaro@users.noreply.github.com>
2023-04-18 13:10:46 +0200

    hello

---
+++ hello.java
@@ -0,0 +1,17 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+// //DEPS <dependency1> <dependency2>
+
+import static java.lang.System.*;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class hello {
+
+    public static void main(String... args) {
+        Arrays.stream(args).forEach(arg ->
+        {
+            System.out.println("Hello " + arg);
+        });
+    }
+}
